4447d7fa0620fd7aac5f2a8b4d8ff325d66f1304
Vharun2005 <vharunsivaraj06@gmail.com>
2024-11-11 21:14:52 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
         <p>© 2024 Time to program ,Happy Coding</p>
             <div className='platform-div'>
                 <Link className="l" to={'https://www.linkedin.com/in/vharun-s-848bb5270/'} target='_blank'><span><FaLinkedin/></span></Link>
-                <Link className="l" to={'https://github.com/Vharun2005/vharun-portfolio'} target='_blank'><span><FaGithub/></span></Link>
+                <Link className="l" to={'https://github.com/Vharun2005/'} target='_blank'><span><FaGithub/></span></Link>
                 <Link className='l' to={'https://www.instagram.com/meet__me__spartan._/?hl=en'} target='_blank'><span><FaInstagram/></span></Link>
             </div>
 
